00f91fb8414bb3d885c8e51aecc7906af1f0d00e
Vincent Novianto <vincentnovianto16@gmail.com>
2023-07-17 12:27:12 +0700

    test

--- src/Materi/Portoo/index.jsx
+++ src/Materi/Portoo/index.jsx
@@ -7,6 +7,7 @@ export default class Komponen extends React.Component{
         return (
             <div>
                 {/* <Porto /> */}
+                tess
             </div>
         )
     }
